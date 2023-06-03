95d20930c6cc143197b85b3bdf3c2c2466540b43
Bryan Ortega <fbryanortega@gmail.com>
2023-06-03 15:41:37 -0500

    finished markov.py

--- markov.py
+++ markov.py
@@ -1,6 +1,7 @@
 """Generate Markov text from text files."""
 
 from random import choice
+import sys
 
 
 def open_and_read_file(file_path):
@@ -10,13 +11,13 @@ def open_and_read_file(file_path):
     the file's contents as one string of text.
     """
 
-    # your code goes here
+    
     text = open(file_path).read()
 
     return text
 
 
-def make_chains(text_string):
+def make_chains(text_string, n):
     """Take input text as string; return dictionary of Markov chains.
 
     A chain will be a key that consists of a tuple of (word1, word2)
@@ -43,53 +44,71 @@ def make_chains(text_string):
 
     chains = {}
     chainwords = text_string.split()
+    # print(chainwords)
+    # chainwords = text_string.split()
 
-    for i in range(len(chainwords) - 2):
-        key = chainwords[i], chainwords[i + 1]
-        value = chainwords[i + 2]
-        
-        if key not in chains:
-            chains[key] = []
+    for i in range(len(chainwords) - int(n)):
+       
+        key_list = []
 
-        chains[key].append(value)
+        for j in range(i, i + int(n)):
+            key_list.append(chainwords[j])
         
-         
+        key_tuple = tuple(key_list)
+
+        value = chainwords[i + int(n)]
+        
+        if key_tuple not in chains:
+            chains[key_tuple] = []
+
+        chains[key_tuple].append(value)
         
     # your code goes here
 
+    # print(chains)
     return chains
 
 
 def make_text(chains):
     """Return text from chains."""
-
-
+    # print(chains)
     words = []
 
     # Step 1: Pick a Starting Point¶
     random_key = choice(list(chains.keys()))
-    words = [random_key[0], random_key[1]]
+    # ('Would', 'you', 'could', 'you', 'in')
+    # print(random_key)
+    words.extend(random_key)
+    # ['Would', 'you', 'could', 'you', 'in']
+    # print(words)
     random_choice = choice(chains[random_key])
+    # ('Would', 'you', 'could', 'you', 'in'): ['a', 'a']
+    # => random_chocie = 'a'
     
     # Step 2: Choose Your Next Word
     while random_key in chains:
-        random_key = (random_key[1], random_choice)
+
+        random_key = tuple(list(random_key[1:]) + [random_choice])
+
         words.append(random_choice)
+
         if random_key in list(chains.keys()):
             random_choice = choice(chains[random_key])
         
-    
-    return ' '.join(words) 
+
     # return words
+    return ' '.join(words) 
 
 
-input_path = 'gettysburg.txt'
+# input_path = 'gettysburg.txt'
 
 # Open the file and turn it into one long string
-input_text = open_and_read_file(input_path)
+input_text = open_and_read_file(sys.argv[1])
+
+n = sys.argv[2]
 
 # Get a Markov chain
-chains = make_chains(input_text)
+chains = make_chains(input_text, n)
 
 # Produce random text
 random_text = make_text(chains)
